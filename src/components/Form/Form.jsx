55666a804e8cd6b0b39f7ad542c955ead35e62e8
Yelyzaveta <elizabethcalmau@gamil.com>
2024-02-20 19:22:49 +0200

    minor changes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 
 export const Form = ({onClick}) => {
     const [inputData, setInputData] = useState({startDate: '', endDate: ''})
-
+    
 
     return <div className='FormWrapper'>
         <form id='tripForm'>
