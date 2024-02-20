c5e0ba24e9adb0be96a7adb30bef49927cb6d125
Yelyzaveta <elizabethcalmau@gamil.com>
2024-02-20 20:23:01 +0200

    send data from form to node

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,27 @@
-
+import { useState } from 'react'
+import axios from 'axios';
 import { X } from 'lucide-react'
 import './Form.css'
-import { useState } from 'react'
 
 export const Form = ({onClick}) => {
     const [inputData, setInputData] = useState({startDate: '', endDate: ''})
     
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    return <div className='FormWrapper'>
-        <form id='tripForm'>
+        axios.post('http://localhost:3000/send', inputData)
+        .then(function (response) {
+            console.log('Response from server:', response.data);
+        })
+        .catch(function (error) {
+        console.error('Error:', error);
+        });
+
+    }
+
+    return <div className='FormWrapper' >
+        <form id='tripForm' onSubmit={handleSubmit}>
             <div className='formLabelWrapper'>
                 <label className='formTitle' htmlFor="tripForm">Create trip</label>
                 <button onClick={onClick}><X/></button>
@@ -26,7 +39,7 @@ export const Form = ({onClick}) => {
             <div>{inputData.startDate}, {inputData.endDate}</div>
             <div className='formButtons'>
                 <button onClick={onClick}>Cancel</button>
-                <button>Save</button>
+                <button type='submit'>Save</button>
             </div>
         </form>
     </div>
