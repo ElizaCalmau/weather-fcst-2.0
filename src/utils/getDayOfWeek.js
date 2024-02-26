e6d725b46811923b5f27dbae1cab78514e0520d0
Yelyzaveta <elizabethcalmau@gamil.com>
2024-02-26 19:02:37 +0200

    test

--- src/utils/getDayOfWeek.js
+++ src/utils/getDayOfWeek.js
@@ -1,5 +1,5 @@
 export function getDayOfWeek(day){
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const dayOfWeek = daysOfWeek[day];
-    return dayOfWeek;
+    return dayOfWeek
 }
